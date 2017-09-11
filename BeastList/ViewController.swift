048da288ba9479c5e115c54b1043915794b7b380
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2017-09-11 16:42:40 -0700

    Cleaned the code by moving the tableView methods inside the UIViewController. Added the "Remove from list" functionality.

--- BeastList/ViewController.swift
+++ BeastList/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     var tasks = ["Something cool", "Something VERY cool", "Something EXTREMELY cool"]
     
@@ -23,6 +23,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
+        tableView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -31,21 +32,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-}
-
-extension ViewController: UITableViewDataSource {
-    // How many cells are we going to need?
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // return an integer that indicates how many rows (cells) to draw
-        return tasks.count
-    }
-    // How should I create each cell?
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // Get the UITableViewCell and create/populate it with data then return it
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
-        cell.textLabel?.text = tasks[indexPath.row]
-        return cell
-    }
-    
+// TableView methods
+//     How many cells are we going to need?
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            // return an integer that indicates how many rows (cells) to draw
+            return tasks.count
+        }
+        // How should I create each cell?
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            // Get the UITableViewCell and create/populate it with data then return it
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+            cell.textLabel?.text = tasks[indexPath.row]
+            return cell
+        }
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            print("Section: \(indexPath.section) and Row: \(indexPath.row)")
+            tasks.remove(at: indexPath.row)
+            tableView.reloadData()
+        }
 }
