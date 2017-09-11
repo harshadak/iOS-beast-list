80a9b75382b41f8598cd3fa21d9b3efa7842b41e
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2017-09-11 16:15:57 -0700

    Finished the assignment.

--- BeastList/ViewController.swift
+++ BeastList/ViewController.swift
@@ -10,14 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var tasks = ["Something cool", "Something VERY cool", "Something EXTREMELY cool"]
+    
     @IBOutlet weak var taskTextField: UITextField!
     @IBOutlet weak var tableView: UITableView!
     @IBAction func beastButtonPressed(_ sender: UIButton) {
-        
+        tasks.append(taskTextField.text!)
+        taskTextField.text = ""
+        tableView.reloadData()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -25,7 +30,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 
 }
 
+extension ViewController: UITableViewDataSource {
+    // How many cells are we going to need?
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // return an integer that indicates how many rows (cells) to draw
+        return tasks.count
+    }
+    // How should I create each cell?
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Get the UITableViewCell and create/populate it with data then return it
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row]
+        return cell
+    }
+    
+}
