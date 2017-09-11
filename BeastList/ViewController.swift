fc31ad1d6ccd44e2efd72bd4f59f24e36bb599ea
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2017-09-11 15:26:22 -0700

    Created the basic story board and added the connections.

--- BeastList/ViewController.swift
+++ BeastList/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var taskTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    @IBAction func beastButtonPressed(_ sender: UIButton) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
